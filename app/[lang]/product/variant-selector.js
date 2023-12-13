b5e6a6fc1891df1c3557325d30b2638dcfbbb290
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-12-13 21:32:43 +0300

    Fixed product options selector logic.

--- app/[lang]/product/variant-selector.js
+++ app/[lang]/product/variant-selector.js
@@ -16,9 +16,11 @@ export function VariantSelector({ lang, product: { meta, variants } }) {
       else options[opt.name] = new Set([opt.value]);
     });
   }
-  Object.keys(options).forEach((name) => (options[name] = Array.from(options[name])));
 
-  return Object.keys(options).map((name, i) => (
+  const optNames = Object.keys(options);
+  optNames.forEach((name) => (options[name] = Array.from(options[name])));
+
+  return optNames.map((name, i) => (
     <dl dir="auto" className="mb-4" key={name}>
       <dt className="mb-2 text-sm uppercase tracking-wide">{optionNames[name][lang] || name}</dt>
       <dd className="flex flex-wrap gap-3">
@@ -37,16 +39,16 @@ export function VariantSelector({ lang, product: { meta, variants } }) {
           const selectedVariant = variants.find((v) =>
             v.options.every((o) => entities.find(([k, v]) => v == o.value))
           );
+
           let img = meta.media.findIndex((m) => m == selectedVariant?.imageUrl);
           img = img < 0 ? 0 : img;
 
           optionSearchParams.set("media", img);
           const optionUrl = createUrl(pathname, optionSearchParams);
-
-          const isAvailableForSale = selectedVariant;
-          const isDisabled = i > 0 && !isAvailableForSale;
-
+          const availableForSale = selectedVariant?.quantity > 0;
+          const isDisabled = !optNames[i + 1] && !availableForSale;
           const isActive = searchParams.get(optionNameLowerCase) === value;
+
           return (
             <button
               key={value}
@@ -67,7 +69,7 @@ export function VariantSelector({ lang, product: { meta, variants } }) {
                 {
                   "cursor-default ring-2 ring-blue-600": isActive,
                   "ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-blue-600 ":
-                    !isActive && isAvailableForSale,
+                    !isActive && availableForSale,
                   "relative z-1 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500 ring-1 ring-neutral-300 before:absolute before:inset-x-0 before:-z-1 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform dark:bg-neutral-900 dark:text-neutral-400 dark:ring-neutral-700 before:dark:bg-neutral-700":
                     isDisabled,
                 }
